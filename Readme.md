faf865043f426bcdbf132c63577b296d527bac56
nikname-1337 <nikhil.kalele9@gmail.com>
2022-07-31 17:28:48 +1000

    simple GET requests working

--- Readme.md
+++ Readme.md
@@ -1,2 +1,7 @@
-# A simple implementation of a expense tracker API in GO.
+## A simple implementation of a expense tracker API in GO.
 ---
+
+Application implements a simple weekly/monthly expense tracker. User can add budgets and expenses, as well as see remaining budget by category.  
+
+
+Runs on localhost:8080 and allows very basic interactions GET, POST, PATCH requests.
--- main.go
+++ main.go
@@ -2,23 +2,45 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type expense struct {
-	ID 	string `json:"id"`
-	Name 	string `json:"name"`
-	Type	string `json:"type"`
-	Value	string `json:"value"`
+	Name     string `json:"name"`
+	Category string `json:"category"`
+	Value    int    `json:"value"`
+	Time     int64  `json:"time"`
 }
 
+type budget struct {
+	Amount    int    `json:"amount"`
+	Category  string `json:"category"`
+	Remaining int    `json:"remaining"`
+}
 
+// Create objects in memory for the sake of this simple demo, in place of a database.
+var budgets = map[string]budget{
+	"Food":      {Amount: 400, Category: "Food", Remaining: 300},
+	"Transport": {Amount: 200, Category: "Transport", Remaining: 200}}
 
+var expenses = map[string]expense{
+	"1": {Name: "McDonalds", Category: "Food", Value: 15, Time: time.Now().Unix()},
+	"2": {Name: "Train", Category: "Transport", Value: 5, Time: time.Now().Unix()}}
+
+func getExpenses(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, expenses)
+}
+
+func getBudgets(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, budgets)
+}
 
 func main() {
 	router := gin.Default()
+	router.GET("/expenses", getExpenses)
+	router.GET("/budgets", getBudgets)
+	router.Run("localhost:8080")
 
 }
-
-
